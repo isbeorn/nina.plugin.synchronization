5b5d7ac407b3fa9ef0921afdfa8d2d7582a442ee
Stefan Berg <isbeorn86@googlemail.com>
2021-12-29 01:33:12 +0100

    Update to latest package

--- Synchronization/Properties/AssemblyInfo.cs
+++ Synchronization/Properties/AssemblyInfo.cs
@@ -34,11 +34,11 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.1.1")]
-[assembly: AssemblyFileVersion("0.2.1.1")]
+[assembly: AssemblyVersion("0.2.1.2")]
+[assembly: AssemblyFileVersion("0.2.1.2")]
 
 //The minimum Version of N.I.N.A. that this plugin is compatible with
-[assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2001")]
+[assembly: AssemblyMetadata("MinimumApplicationVersion", "2.0.0.2020")]
 
 //Your plugin homepage - omit if not applicaple
 [assembly: AssemblyMetadata("Homepage", "https://www.patreon.com/stefanberg/")]
